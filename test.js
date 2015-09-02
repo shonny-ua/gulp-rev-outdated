7859106af13b662fde20014ae4753f4695483b99
Alex Rothenberg <alex@alexrothenberg.com>
2015-09-02 11:38:07 -0400

    tests check for exact files to pass through stream

--- test.js
+++ test.js
@@ -61,11 +61,45 @@ var keepQuantity;
 var uniqueFiles = 5;
 var filteredQuantity;
 var fileCount;
+var actualFiles;
 
 it('should filter 30 files', function (cb) {
     keepQuantity = 1;
-    filteredQuantity = 7 * uniqueFiles - keepQuantity * uniqueFiles;
     fileCount = 0;
+    actualFiles = [];
+    var expectedOutdatedFiles = [
+        "css/style-61e0be79.css",
+        "css/style-a42f5380.css",
+        "css/style-1d87bebe.css",
+        "css/style-11111111.css",
+        "css/style-11111111.min.css",
+        "css/style-00000000.css",
+        "css/vendor-61e0be79.css",
+        "css/vendor-a42f5380.css",
+        "css/vendor-1d87bebe.css",
+        "css/vendor-11111111.css",
+        "css/vendor-11111111.min.css",
+        "css/vendor-00000000.css",
+        "css/vendor-61e0be79.js",
+        "css/vendor-a42f5380.js",
+        "css/vendor-1d87bebe.js",
+        "css/vendor-11111111.js",
+        "css/vendor-11111111.min.js",
+        "css/vendor-00000000.js",
+        "css/fonts/fontstyle-61e0be79.css",
+        "css/fonts/fontstyle-a42f5380.css",
+        "css/fonts/fontstyle-1d87bebe.css",
+        "css/fonts/fontstyle-11111111.css",
+        "css/fonts/fontstyle-11111111.min.css",
+        "css/fonts/fontstyle-00000000.css",
+        "css/try-to-trip-regex-61e0be79.css",
+        "css/try-to-trip-regex-a42f5380.css",
+        "css/try-to-trip-regex-1d87bebe.css",
+        "css/try-to-trip-regex-11111111.css",
+        "css/try-to-trip-regex-11111111.min.css",
+        "css/try-to-trip-regex-00000000.css"
+    ];
+    filteredQuantity = expectedOutdatedFiles.length;
 
     var stream = initStream(revOutdated(keepQuantity));
 
@@ -73,6 +107,7 @@ it('should filter 30 files', function (cb) {
 
     stream.on('end', function () {
         assert.equal(fileCount, filteredQuantity, 'Only ' + filteredQuantity + ' files should pass through the stream');
+        assert.deepEqual(actualFiles, expectedOutdatedFiles);
         cb();
     });
 
@@ -81,8 +116,36 @@ it('should filter 30 files', function (cb) {
 
 it('should filter 25 files of different types using default keepQuantity option', function (cb) {
     keepQuantity = undefined;
-    filteredQuantity = 7 * uniqueFiles - 2 * uniqueFiles;
     fileCount = 0;
+    actualFiles = [];
+    var expectedOutdatedFiles = [
+        "css/style-a42f5380.css",
+        "css/style-1d87bebe.css",
+        "css/style-11111111.css",
+        "css/style-11111111.min.css",
+        "css/style-00000000.css",
+        "css/vendor-a42f5380.css",
+        "css/vendor-1d87bebe.css",
+        "css/vendor-11111111.css",
+        "css/vendor-11111111.min.css",
+        "css/vendor-00000000.css",
+        "css/vendor-a42f5380.js",
+        "css/vendor-1d87bebe.js",
+        "css/vendor-11111111.js",
+        "css/vendor-11111111.min.js",
+        "css/vendor-00000000.js",
+        "css/fonts/fontstyle-a42f5380.css",
+        "css/fonts/fontstyle-1d87bebe.css",
+        "css/fonts/fontstyle-11111111.css",
+        "css/fonts/fontstyle-11111111.min.css",
+        "css/fonts/fontstyle-00000000.css",
+        "css/try-to-trip-regex-a42f5380.css",
+        "css/try-to-trip-regex-1d87bebe.css",
+        "css/try-to-trip-regex-11111111.css",
+        "css/try-to-trip-regex-11111111.min.css",
+        "css/try-to-trip-regex-00000000.css"
+    ];
+    filteredQuantity = expectedOutdatedFiles.length;
 
     var stream = initStream(revOutdated());
 
@@ -96,6 +159,7 @@ it('should filter 25 files of different types using default keepQuantity option'
 
     stream.on('end', function () {
         assert.equal(fileCount, filteredQuantity, 'Only ' + filteredQuantity + ' files should pass through the stream');
+        assert.deepEqual(actualFiles, expectedOutdatedFiles);
         cb();
     });
 
@@ -104,8 +168,31 @@ it('should filter 25 files of different types using default keepQuantity option'
 
 it('should filter correct files', function (cb) {
     keepQuantity = 3;
-    filteredQuantity = 7 * uniqueFiles - keepQuantity * uniqueFiles;
     fileCount = 0;
+    actualFiles = [];
+    var expectedOutdatedFiles = [
+        "css/style-1d87bebe.css",
+        "css/style-11111111.css",
+        "css/style-11111111.min.css",
+        "css/style-00000000.css",
+        "css/vendor-1d87bebe.css",
+        "css/vendor-11111111.css",
+        "css/vendor-11111111.min.css",
+        "css/vendor-00000000.css",
+        "css/vendor-1d87bebe.js",
+        "css/vendor-11111111.js",
+        "css/vendor-11111111.min.js",
+        "css/vendor-00000000.js",
+        "css/fonts/fontstyle-1d87bebe.css",
+        "css/fonts/fontstyle-11111111.css",
+        "css/fonts/fontstyle-11111111.min.css",
+        "css/fonts/fontstyle-00000000.css",
+        "css/try-to-trip-regex-1d87bebe.css",
+        "css/try-to-trip-regex-11111111.css",
+        "css/try-to-trip-regex-11111111.min.css",
+        "css/try-to-trip-regex-00000000.css"
+    ];
+    filteredQuantity = expectedOutdatedFiles.length;
 
     var stream = initStream(revOutdated(keepQuantity));
 
@@ -119,6 +206,7 @@ it('should filter correct files', function (cb) {
 
     stream.on('end', function () {
         assert.equal(fileCount, filteredQuantity, 'Only ' + filteredQuantity + ' files should pass through the stream');
+        assert.deepEqual(actualFiles, expectedOutdatedFiles);
         cb();
     });
 
@@ -142,4 +230,5 @@ function streamDataCheck(file) {
         'should filter only revisioned files'
     );
     fileCount++;
+    actualFiles.push(file.path);
 }
